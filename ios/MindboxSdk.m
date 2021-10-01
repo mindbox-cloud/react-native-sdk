b85129a6499e64337cecb13a349ebb9cef4826e4
umbrellait-nikolay-seleznev <nikolay.seleznev@umbrellait.com>
2021-10-01 14:11:41 +0300

    chore: add basic methods (initialize, getDeviceUUID, getToken, updateToken)

--- ios/MindboxSdk.m
+++ ios/MindboxSdk.m
@@ -2,6 +2,12 @@
 
 @interface RCT_EXTERN_MODULE(MindboxSdk, NSObject)
 
-RCT_EXTERN_METHOD(initialize:(NSString)domain endpoint:(NSString)endpoint)
+RCT_EXTERN_METHOD(initialize:(NSString)payloadString resolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getDeviceUUID:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getAPNSToken:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(updateAPNSToken:(NSString)token resolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 
 @end
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,172 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
-type MindboxSdkType = {
-  initialize(domain: String, endpoint: String): void;
+const { MindboxSdk: MindboxSdkNative } = NativeModules;
+
+type InitializationData = {
+  domain: string;
+  endpointId: string;
+  subscribeCustomerIfCreated: boolean;
+  shouldCreateCustomer?: boolean;
+  previousInstallId?: string;
+  previousUuid?: string;
 };
 
-const { MindboxSdk } = NativeModules;
+type GetDeviceUUIDCallback = (deviceUUID: string) => any;
 
-export default MindboxSdk as MindboxSdkType;
+type GetTokenCallback = (token: string) => any;
+
+class MindboxSdkClass {
+  private _initialized: boolean;
+  private _initializing: boolean;
+  private _callbacks: Array<() => void>;
+
+  constructor() {
+    this._initialized = false;
+    this._initializing = false;
+    this._callbacks = [];
+  }
+
+  get initialized() {
+    return this._initialized;
+  }
+
+  public async initialize(initializationData: InitializationData) {
+    if (this._initializing) {
+      return;
+    }
+
+    this._initializing = true;
+
+    if (this._initialized) {
+      this._initializing = false;
+      return;
+    }
+
+    const {
+      domain,
+      endpointId,
+      subscribeCustomerIfCreated,
+      shouldCreateCustomer,
+      previousInstallId,
+      previousUuid,
+    } = initializationData;
+
+    const payload: InitializationData = {
+      domain,
+      endpointId,
+      subscribeCustomerIfCreated,
+    };
+
+    if (typeof shouldCreateCustomer !== 'undefined') {
+      payload.shouldCreateCustomer = shouldCreateCustomer;
+    }
+
+    if (
+      typeof previousInstallId !== 'undefined' &&
+      previousInstallId.length > 0
+    ) {
+      payload.previousInstallId = previousInstallId;
+    }
+
+    if (typeof previousUuid !== 'undefined' && previousUuid.length > 0) {
+      payload.previousUuid = previousUuid;
+    }
+
+    try {
+      const payloadString = JSON.stringify(payload);
+      this._initialized = await MindboxSdkNative.initialize(payloadString);
+      this._initializing = false;
+
+      for (let i = 0; i < this._callbacks.length; i++) {
+        this._callbacks[i]();
+      }
+
+      this._callbacks = [];
+    } catch (error) {
+      this._initializing = false;
+      throw error;
+    }
+  }
+
+  public getDeviceUUID(callback: GetDeviceUUIDCallback) {
+    if (typeof callback !== 'function') {
+      return;
+    }
+
+    const callbackHandler = () => {
+      MindboxSdkNative.getDeviceUUID().then((deviceUUID: string) =>
+        callback(deviceUUID)
+      );
+    };
+
+    if (this._initialized) {
+      callbackHandler();
+    } else {
+      this._callbacks.push(callbackHandler);
+    }
+  }
+
+  public getToken(callback: GetTokenCallback) {
+    if (typeof callback !== 'function') {
+      return;
+    }
+
+    const callbackHandler = () => {
+      let promise = null;
+
+      switch (Platform.OS) {
+        case 'ios':
+          promise = MindboxSdkNative.getAPNSToken();
+          break;
+
+        case 'android':
+          promise = MindboxSdkNative.getFMSToken();
+          break;
+
+        default:
+          promise = MindboxSdkNative.getAPNSToken();
+          break;
+      }
+
+      promise.then((token: string) => callback(token));
+    };
+
+    if (this._initialized) {
+      callbackHandler();
+    } else {
+      this._callbacks.push(callbackHandler);
+    }
+  }
+
+  public async updateToken(token: string) {
+    switch (Platform.OS) {
+      case 'ios':
+        try {
+          await MindboxSdkNative.updateAPNSToken(token);
+        } catch (error) {
+          throw error;
+        }
+        break;
+
+      case 'android':
+        try {
+          await MindboxSdkNative.updateFMSToken(token);
+        } catch (error) {
+          throw error;
+        }
+        break;
+
+      default:
+        try {
+          await MindboxSdkNative.updateAPNSToken(token);
+        } catch (error) {
+          throw error;
+        }
+        break;
+    }
+  }
+}
+
+const MindboxSdk = new MindboxSdkClass();
+
+export default MindboxSdk;
