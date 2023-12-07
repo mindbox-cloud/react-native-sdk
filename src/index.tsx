c7be146b4cdca4dc2ce00f4d79a03ff868b5105a
Robert Alimbekov <robert.alimbekov@rentateam.ru>
2023-12-07 20:45:10 +0500

    feat(callbacks): callbacks

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import type {
 } from './types';
 import {EmitterSubscription, NativeEventEmitter, NativeModules, Platform,} from 'react-native';
 import type {InAppCallback} from "./InAppCallback";
-import type {ExecuteAsyncOperationPayload, ExecuteSyncOperationPayload, InitializationData,} from './types';
 
 
 const {MindboxSdk: MindboxSdkNative, MindboxJsDelivery} = NativeModules;
