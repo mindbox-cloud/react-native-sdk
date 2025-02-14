a36fc362076841f9f9789a78528ae324072f09aa
sozinov <sozinov@mindbox.cloud>
2025-02-14 13:50:37 +0300

    WMRUSTORE-38: fix RuStoreMessagingService

--- example/exampleApp/android/app/src/main/java/com/exampleapp/MindboxRuStoreMessagingService.kt
+++ example/exampleApp/android/app/src/main/java/com/exampleapp/MindboxRuStoreMessagingService.kt
@@ -2,13 +2,10 @@ package com.exampleapp
 
 import cloud.mindbox.mindbox_rustore.MindboxRuStore
 import cloud.mindbox.mobile_sdk.Mindbox
-import kotlinx.coroutines.*
 import ru.rustore.sdk.pushclient.messaging.model.RemoteMessage
 import ru.rustore.sdk.pushclient.messaging.service.RuStoreMessagingService
 
-class MindboxRuStoreMessagingService: RuStoreMessagingService() {
-
-    private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+class MindboxRuStoreMessagingService : RuStoreMessagingService() {
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
@@ -22,40 +19,35 @@ class MindboxRuStoreMessagingService: RuStoreMessagingService() {
         val channelDescription = "default_channel_description"
         val pushSmallIcon = android.R.drawable.ic_dialog_info
 
-        // On some devices, onMessageReceived may be executed on the main thread
-        // We recommend handling push messages asynchronously
-        coroutineScope.launch {
-            val messageWasHandled = Mindbox.handleRemoteMessage(
-                context = applicationContext,
-                message = remoteMessage,
-                activities = mapOf(),
-                channelId = channelId,
-                channelName = channelName,
-                pushSmallIcon = pushSmallIcon,
-                defaultActivity = MainActivity::class.java,
-                channelDescription = channelDescription
-            )
+        val messageWasHandled = Mindbox.handleRemoteMessage(
+            context = applicationContext,
+            message = remoteMessage,
+            activities = mapOf(),
+            channelId = channelId,
+            channelName = channelName,
+            pushSmallIcon = pushSmallIcon,
+            defaultActivity = MainActivity::class.java,
+            channelDescription = channelDescription
+        )
 
-            // Method for checking if push is from Mindbox
-            val isMindboxPush = MindboxRuStore.isMindboxPush(remoteMessage = remoteMessage)
+        // Method for checking if push is from Mindbox
+        val isMindboxPush = MindboxRuStore.isMindboxPush(remoteMessage = remoteMessage)
 
-            // Method for getting info from Mindbox push
-            val mindboxMessage =
-                MindboxRuStore.convertToMindboxRemoteMessage(remoteMessage = remoteMessage)
-            // If you want to save the notification you can call your save function from here.
-            mindboxMessage?.let {
-                val app = applicationContext as MainApplication
-                app.saveNotification(it)
-            }
+        // Method for getting info from Mindbox push
+        val mindboxMessage =
+            MindboxRuStore.convertToMindboxRemoteMessage(remoteMessage = remoteMessage)
+        // If you want to save the notification you can call your save function from here.
+        mindboxMessage?.let {
+            val app = applicationContext as MainApplication
+            app.saveNotification(it)
+        }
 
-            if (!messageWasHandled) {
-                //If the push notification was not from Mindbox or it contains incorrect data, then you can write a fallback to process it
-            }
+        if (!messageWasHandled) {
+            //If the push notification was not from Mindbox or it contains incorrect data, then you can write a fallback to process it
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        coroutineScope.cancel()
     }
 }
