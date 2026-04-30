86285b3c9911000b128180357c8c36dde802fb9d
sozinov <sozinov@mindbox.cloud>
2026-04-30 13:42:26 +0300

    MOBILE-106: Fix iOS New Architecture guard and Swift header import

--- ios/MindboxSdkNewArchGuard.mm
+++ ios/MindboxSdkNewArchGuard.mm
@@ -1,3 +1,3 @@
-#if defined(RCT_NEW_ARCH_ENABLED) && RCT_NEW_ARCH_ENABLED != 1
+#if !defined(RCT_NEW_ARCH_ENABLED) || RCT_NEW_ARCH_ENABLED != 1
 #error "MindboxSdk requires React Native New Architecture. Set RCT_NEW_ARCH_ENABLED=1."
 #endif
